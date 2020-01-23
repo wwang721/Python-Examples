0d753139fb78825dc5e384b08520f571d2a710b6
Wei Wang <w_w19980721@foxmail.com>
2020-01-23 15:20:22 +0800

    Add files via upload

---
+++ CSV/CPP/csv_load.cpp
@@ -0,0 +1,83 @@
+//
+//  csv_load.cpp
+//  CPP
+//
+//  Created by WW on 2020/01/23.
+//  Copyright © 2020 Wang Wei. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>	// stringstream
+#include <stdio.h>	// sscanf
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	string fileName("lambda.csv");
+	
+	vector< vector<string> > dataString; // data in 2-D String vector
+	
+
+	ifstream fin(fileName);
+	if(!fin)
+	{	
+		cout<<"Error: <"<<fileName<<"> is not existed!\n";
+		/*
+		ofstream fout(fileName);
+
+		fout_gx<<S[i]<<",";	
+		
+		fout.close();
+		*/
+	}
+	else
+	{
+		cout<<"Loading <"<<fileName<<"> ...\n";
+		
+		string line;	// every line data in String
+		
+		while (getline(fin, line)) 
+		{
+			stringstream ss(line); 	// convert "string" to "stringstream" for getline() function
+			
+			string data;	// data in String
+			
+			vector<string> linedataString;	// every line data in String vector
+			
+			while (getline(ss, data, ','))	// delimiter=','
+				linedataString.push_back(data);
+
+			dataString.push_back(linedataString);
+     	}
+	}
+	fin.close();
+
+	int dimension[2];
+	dimension[0] = dataString.size();
+	dimension[1] = dataString[0].size();
+
+	cout<<"Data dimension: "<<dimension[0]<<"x"<<dimension[1]<<endl;
+
+	//data in 2-D vector S(m*n)
+	vector< vector<double> > S(dimension[0], vector<double>(dimension[1]));
+
+	for(int i = 0; i < dimension[0]; i++)
+	{
+		for(int j = 0; j < dimension[1]; j++)
+		{			
+			// c_str() covert "string" to "char *"
+			// sscanf() convert "char *" to "%lf" ("double")			
+			sscanf(dataString[i][j].c_str(), "%lf", &S[i][j]);	
+			cout<<S[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+	
+}
---
+++ CSV/np_load_save.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+# Created by WW on Jan. 23, 2020
+# All rights reserved.
+#
+
+import numpy as np 
+
+def main():
+	#=============================================================
+	''' load plain text file (.txt) as np.array '''
+	data = np.loadtxt(r"lambda.txt")
+
+	#print(data)
+
+	x = data[:,0] # get the first column of data
+	y = data[:,1] # get the second column of data
+
+	#print(x)
+	#print(y)
+
+	#=============================================================
+	''' load .csv (Comma Separated Values) file as np.array '''
+	np.savetxt("lambda.csv", data, fmt="%g", delimiter=',')
+
+	data1 = np.loadtxt(r"lambda.csv", delimiter=',')
+	#print(data1)
+
+	#=============================================================
+	''' load .tsv (Tab Separated Values) file as np.array '''
+	np.savetxt("lambda.tsv", data, fmt="%g", delimiter='\t')
+
+	data2 = np.loadtxt(r"lambda.tsv", delimiter='\t')
+	#print(data2)
+
+if __name__ == "__main__":
+    main()
