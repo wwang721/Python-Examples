f26d52a3dd623c63bd0cb698b8854dd7de4026ea
Wei Wang <w_w19980721@foxmail.com>
2020-01-27 14:27:18 +0800

    Add files via upload

--- inset_plot.py
+++ inset_plot.py
@@ -26,11 +26,11 @@ y = data[:,1]
 
 #-----------------------------------------------------------------------------
 
-fig = plt.figure(figsize=(8, 6), dpi=300)
+fig = plt.figure(figsize=(8, 6))
 
 # 设置xtick和ytick的方向：in、out、inout
 plt.rcParams['xtick.direction'] = 'in'
-plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'out'
 
 Formatter = FormatStrFormatter('%g') # tick label format
 
@@ -43,13 +43,12 @@ ax1.plot(x, y,  "-o", color="tomato", linewidth=1.5)
 
 ax1.set_xlabel(r"$T/K$")
 ax1.set_ylabel(r"$\lambda/\mu m$")
+ax1.set_title(r"$\lambda$-$T$ Figure")
 
 ax1.set_xlim(0,300)
 ax1.set_ylim(5.8,8.4)
 ax1.set_xticks(np.arange(25,300,50), minor="True")	# show minor ticks
 ax1.set_yticks(np.arange(6.25,8.26,0.5), minor="True") # show minor ticks
-
-ax1.tick_params(axis='both', which="both", top=True, right=True) # show XY major and minor ticks on top and right
 
 ax1.grid(True) # show major tick grid 
 ax1.grid(True, which="minor", linestyle="--") # show minor tick grid
@@ -60,16 +59,14 @@ ax1.grid(True, which="minor", linestyle="--") # show minor tick grid
 #ax2 = inset_axes(ax1, width=3, height=2.3, loc="upper right") # use inset_axes() directly
 
 ax2 = fig.add_axes([0.5,0.5,0.385,0.36]) # position: left, bottom, width, height (range 0 to 1)
+
 ax2.plot(x, y, "-o", color="deepskyblue", linewidth=1.5)
-ax2.set_xlim(7,25)
-ax2.set_ylim(7.25,8.25)
+ax2.set_xlim(10,60)
+ax2.set_ylim(7.35,8.25)
 
 ax2.xaxis.set_major_formatter(Formatter) # major ticks label format
-ax2.set_xticks(np.arange(7.5,25,5), minor="True") # show minor ticks
-ax2.set_yticks(np.arange(7.3,8.2,0.2), minor="True") # show minor ticks
-ax2.xaxis.set_minor_formatter(Formatter) # show minor ticks label
-
-ax2.tick_params(axis='both', which="both", top=True, right=True) # show XY major and minor ticks on top and right
+ax2.set_xticks(np.arange(15,60,10), minor="True") # show minor ticks
+ax2.set_yticks(np.arange(7.5,8.2,0.2), minor="True") # show minor ticks
 
 ax2.grid(True, linestyle="-.") # show major tick grid
 #ax2.grid(True, which="minor", linestyle="--")
