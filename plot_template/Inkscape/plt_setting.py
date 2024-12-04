d3206cff87e1bc3b69ae36755e01596671d38201
Wei Wang <ww000721@gmail.com>
2024-12-04 11:40:58 -0500

    Inkscape support :sparkles:

---
+++ plot_template/Inkscape/plt_setting.py
@@ -0,0 +1,29 @@
+#
+#   Created by WW on 2023-12-20.
+#   Copyright © WW. All rights reserved.
+#
+#   Basic plot settings
+#
+
+import matplotlib.pyplot as plt
+
+
+def plt_init():
+    plt.rcParams.update({
+        # requires a working LaTeX installation & slower than Matplotlib's mathtext
+        "text.usetex": False,
+        "font.family": "Times New Roman",    #"sans-serif"
+        # "font.sans-serif": "Times New Roman"# "DejaVu Sans"
+    })
+    
+    plt.rcParams['svg.fonttype'] = 'none'    # save svg without text as paths but embedded fonts
+    plt.rcParams["mathtext.fontset"] = "cm"    # change the math font to Computer Modern (TeX font)
+    plt.rcParams['figure.constrained_layout.use'] = True
+    plt.rcParams['ytick.right'] = True
+    plt.rcParams['xtick.top'] = True
+    plt.rc('xtick', labelsize=12, direction='in')
+    plt.rc('ytick', labelsize=12, direction='in')
+    plt.rc('axes', labelsize=14, titlesize=14)
+    plt.rc('legend', fontsize=12)
+    
+
